24f6944d180e486f823aa9a787ac3634ed490f94
Larry Okeke <44207903+okekelarry@users.noreply.github.com>
2019-04-26 15:37:05 -0400

    Update camera.py

--- Python/camera.py
+++ Python/camera.py
@@ -6,6 +6,6 @@ camera = piCamera()
 camera.rotation = 180
 camera.start_preview()
 for i in range(5):
-	sleep(5) #gives camera time to get redy to take the shot
+	sleep(5) #gives camera time to get ready to take the shot
 	camera.capture('/home/pi/Desktop/image%s.jpg' % i)
 camera.stop_preview()
